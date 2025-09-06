f27693e394492332a51575beca5c0d40e18ddee6
Manoj <ermanoj.manit@gmail.com>
2025-09-07 00:26:49 +0530

    fixed the path

--- src/main/app.js
+++ src/main/app.js
@@ -1,8 +1,8 @@
 function onSelect(e) {
     console.log("ON select called: " + e.target.value);
-    window.location.href = "../station/" + e.target.value + ".html";
+    window.location.href = "/src/station/" + e.target.value + ".html";
 }
 
 function backToHome() {
-    window.location.href = "../index.html";
+    window.location.href = "../../index.html";
 }
