b780c397d0b814513d256a8020728679bbb2e61b
Manoj <ermanoj.manit@gmail.com>
2025-09-08 17:17:35 +0530

    added functions and styles

---
+++ src/main/app.js
@@ -0,0 +1,8 @@
+function onSelect(e) {
+    console.log("ON select called: " + e.target.value);
+    window.location.href = "/src/station/" + e.target.value + ".html";
+}
+
+function backToHome() {
+    window.location.href = "../../index.html";
+}
