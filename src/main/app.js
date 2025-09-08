b6133433c8ae53de3aefcb563a9ea9c5881bbe24
Manoj <ermanoj.manit@gmail.com>
2025-09-08 16:52:27 +0530

    git pages issue

--- src/main/app.js
+++
@@ -1,8 +0,0 @@
-function onSelect(e) {
-    console.log("ON select called: " + e.target.value);
-    window.location.href = "/src/station/" + e.target.value + ".html";
-}
-
-function backToHome() {
-    window.location.href = "../../index.html";
-}
