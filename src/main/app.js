ef5d58aa3157dc17fcb1c227fc3118756b5ff735
Manoj <ermanoj.manit@gmail.com>
2025-09-09 10:55:14 +0530

    fixed app.js

--- src/main/app.js
+++ src/main/app.js
@@ -1,6 +1,6 @@
 function onSelect(e) {
     console.log("ON select called: " + e.target.value);
-    window.location.href = "/src/station/" + e.target.value + ".html";
+    window.location.href = "./src/station/" + e.target.value + ".html";
 }
 
 function backToHome() {
