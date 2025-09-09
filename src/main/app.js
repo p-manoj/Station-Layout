344335fabcd4d21af65ed1443d577dbd794bba3c
Manoj <ermanoj.manit@gmail.com>
2025-09-09 12:25:42 +0530

    optimised with spa approach

--- src/main/app.js
+++ src/main/app.js
@@ -1,8 +1,18 @@
+let selectedStation;
+
 function onSelect(e) {
-    console.log("ON select called: " + e.target.value);
-    window.location.href = "./src/station/" + e.target.value + ".html";
+    selectedStation = e.target.value;
+    // window.location.href = "./src/station/" + e.target.value + ".html";
+    document.getElementById("stationList").classList.add("hidden");
+    document.getElementById("backbtn").classList.remove("hidden");
+    document.getElementById(selectedStation).classList.remove("hidden");
+    e.target.value = '';
 }
 
 function backToHome() {
-    window.location.href = "../../index.html";
+    // window.location.href = "../../index.html";
+    document.getElementById("backbtn").classList.add("hidden");
+    document.getElementById(selectedStation).classList.add("hidden");
+    document.getElementById("stationList").classList.remove("hidden");
+
 }
